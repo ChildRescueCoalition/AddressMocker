215845b3a328576e854c1cfd43f7c6be2473d4e3
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2024-05-14 16:49:39 -0400

    Initial AddressMocker.
    Support for IPv4, conversion to Int and Boolean hashing.

--- Sources/AddressMocker/AddressMocker.swift
+++ Sources/AddressMocker/AddressMocker.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// AddressMocker
+// Support library for Testing and Mocking services.
+
+import Foundation
+import Network
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+public enum AddressMockerError: Error {
+	case invalidAddress
+}
+
+public struct AddressMocker {
+	let IPv4: IPv4Address
+
+	public init(usingIPv4 address: String) throws {
+		guard let address = IPv4Address(address) else {
+			throw AddressMockerError.invalidAddress
+		}
+		self.IPv4 = address
+	}
+
+	public var integer: UInt32 {
+		IPv4.rawValue.withUnsafeBytes { $0.load(as: UInt32.self) }
+	}
+
+	public var boolean: Bool {
+		self.integer.isMultiple(of: 2)
+	}
+}
--- Tests/AddressMockerTests/AddressMockerTests.swift
+++ Tests/AddressMockerTests/AddressMockerTests.swift
@@ -2,11 +2,33 @@ import XCTest
 @testable import AddressMocker
 
 final class AddressMockerTests: XCTestCase {
-	func testExample() throws {
-		// XCTest Documentation
-		// https://developer.apple.com/documentation/xctest
+	func testCreateValidAM() throws {
+		XCTAssertNoThrow(try AddressMocker(usingIPv4: "127.0.0.1"))
+		XCTAssertNoThrow(try AddressMocker(usingIPv4: "192.168.1.1"))
+		XCTAssertNoThrow(try AddressMocker(usingIPv4: "8.8.8.8"))
+		XCTAssertNoThrow(try AddressMocker(usingIPv4: "4.4.4.4"))
+		XCTAssertNoThrow(try AddressMocker(usingIPv4: "255.255.255.255"))
+	}
 
-		// Defining Test Cases and Test Methods
-		// https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+	func testCreateInvalidAM() throws {
+		XCTAssertThrowsError(try AddressMocker(usingIPv4: "invalid"))
+	}
+
+	func testAMIntegerValue() throws {
+		XCTAssertEqual(try AddressMocker(usingIPv4: "127.0.0.1").integer, 0x100007F)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "192.168.1.1").integer, 16885952)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "8.8.8.8").integer, 134744072)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "4.4.4.4").integer, 67372036)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "10.0.0.1").integer, 16777226)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "255.255.255.255").integer, 4294967295)
+	}
+
+	func testAMBooleanValue() throws {
+		XCTAssertEqual(try AddressMocker(usingIPv4: "127.0.0.1").boolean, false)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "192.168.1.1").boolean, true)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "8.8.8.8").boolean, true)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "4.4.4.4").boolean, true)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "10.0.0.1").boolean, true)
+		XCTAssertEqual(try AddressMocker(usingIPv4: "255.255.255.255").boolean, false)
 	}
 }
